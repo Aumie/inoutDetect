01acd2f60b64bb8a929b588d2fc19ddedb454e87
Aumie <aum5376@gmail.com>
2020-07-20 15:45:19 +0700

    isolated database

--- database/initialdb.py
+++ database/initialdb.py
@@ -25,15 +25,43 @@ def data_today():
     global ref
     global firebase
     # cam0 initialize data
-    if firebase.child('in').get() is None:
-        firebase.update({'in': 0})
+    if firebase.child('in0').get() is None:
+        firebase.update({'in0': 0})
     else:
-        isCamsOpen.p_in = firebase.child('in').get()
-    if firebase.child('out').get() is None:
-        firebase.update({'out': 0})
+        isCamsOpen.p_in = firebase.child('in0').get()
+    if firebase.child('out0').get() is None:
+        firebase.update({'out0': 0})
     else:
         isCamsOpen.p_out = firebase.child('out').get()
     if firebase.child('time_end').get() is None:
         firebase.update({'time_end': str(datetime.now().strftime("%H:%M:%S"))})
     if firebase.child('time_start').get() is None:
         firebase.update({'time_start': str(datetime.now().strftime("%H:%M:%S"))})
+    # cam1 initialize data
+    if firebase.child('in1').get() is None:
+        firebase.update({'in1': 0})
+    else:
+        isCamsOpen.p_in1 = firebase.child('in1').get()
+    if firebase.child('out1').get() is None:
+        firebase.update({'out1': 0})
+    else:
+        isCamsOpen.p_out1 = firebase.child('out1').get()
+    # cam2 initialize data
+    if firebase.child('in2').get() is None:
+        firebase.update({'in2': 0})
+    else:
+        isCamsOpen.p_in2 = firebase.child('in2').get()
+    if firebase.child('out2').get() is None:
+        firebase.update({'out2': 0})
+    else:
+        isCamsOpen.p_out2 = firebase.child('out2').get()
+    # cam3 initialize data
+    if firebase.child('in3').get() is None:
+        firebase.update({'in3': 0})
+    else:
+        isCamsOpen.p_in3 = firebase.child('in3').get()
+    if firebase.child('out3').get() is None:
+        firebase.update({'out3': 0})
+    else:
+        isCamsOpen.p_out3 = firebase.child('out3').get()
+
--- database/updateToFirebase.py
+++ database/updateToFirebase.py
@@ -11,6 +11,16 @@ def loop_update_firebase():
 
 
 def update_data():
-    initialdb.firebase.update({'in': isCamsOpen.p_in})
-    initialdb.firebase.update({'out': isCamsOpen.p_out})
+    if isCamsOpen.iscamopen:
+        initialdb.firebase.update({'in0': isCamsOpen.p_in})
+        initialdb.firebase.update({'out0': isCamsOpen.p_out})
+    elif isCamsOpen.iscam1open:
+        initialdb.firebase.update({'in1': isCamsOpen.p_in1})
+        initialdb.firebase.update({'out1': isCamsOpen.p_out1})
+    elif isCamsOpen.iscam2open:
+        initialdb.firebase.update({'in2': isCamsOpen.p_in2})
+        initialdb.firebase.update({'out2': isCamsOpen.p_out2})
+    elif isCamsOpen.iscam3open:
+        initialdb.firebase.update({'in3': isCamsOpen.p_in3})
+        initialdb.firebase.update({'out3': isCamsOpen.p_out3})
     initialdb.firebase.update({'time_end': str(datetime.now().strftime("%H:%M:%S"))})
