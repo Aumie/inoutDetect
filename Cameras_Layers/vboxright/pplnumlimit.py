bffe6a7539d46e11f7d22e005f49babfbbe03278
Aumie <aum5376@gmail.com>
2020-09-14 11:52:22 +0700

    fix notify and directdisplay

--- Cameras_Layers/vboxright/pplnumlimit.py
+++ Cameras_Layers/vboxright/pplnumlimit.py
@@ -26,14 +26,14 @@ def spin_update(self):
         isCamsOpen.limit2 = self.spinBox.value()
     if self.cameranum == 3:
         isCamsOpen.limit3 = self.spinBox.value()
-    # print(isCamsOpen.limit)
+    print(isCamsOpen.limit)
 
 
-linehistory = [[0, 0], [0, 0]]  # total and limit ,index 0 = new, 1 = old
+sent = False
 
 
 def line_notify(self):  # also set status
-    global linehistory
+    global sent
     context = """Entrance{0} has reached the limit!!!
 Status: In -------- {1}
           Out ------- {2}
@@ -43,39 +43,47 @@ Status: In -------- {1}
     getout = 0
     total = 0
     limit = 0
+    direct = 'lr'
     if self.cameranum == 0:
+        limit = isCamsOpen.limit
         getin = isCamsOpen.p_in
         getout = isCamsOpen.p_out
-        total = getin - getout
-        limit = isCamsOpen.limit
+        direct = isCamsOpen.camdirect
+
     if self.cameranum == 1:
+        limit = isCamsOpen.limit1
         getin = isCamsOpen.p_in1
         getout = isCamsOpen.p_out1
-        total = getin - getout
-        limit = isCamsOpen.limit1
+        direct = isCamsOpen.camdirect1
+
     if self.cameranum == 2:
+        limit = isCamsOpen.limit2
         getin = isCamsOpen.p_in2
         getout = isCamsOpen.p_out2
-        total = getin - getout
-        limit = isCamsOpen.limit2
+        direct = isCamsOpen.camdirect2
+
     if self.cameranum == 3:
+        limit = isCamsOpen.limit3
         getin = isCamsOpen.p_in3
         getout = isCamsOpen.p_out3
-        total = getin - getout
-        limit = isCamsOpen.limit3
+        direct = isCamsOpen.camdirect3
 
-    linehistory[0].pop()
-    linehistory[0].insert(0, total)
-    if limit > 0:
-        print(linehistory)
-        linehistory[1].pop()
-        linehistory[1].insert(0, limit)
 
-        if linehistory[0][0] != linehistory[0][1] or linehistory[1][0] != linehistory[1][1]:
-            if total >= limit:
-                self.status.setText('Full')
+    total = abs(getin - getout)
+
+    # print(limit, isCamsOpen.limit)
+
+    if total < limit != 0:
+        self.status.setText('Good')
+        sent = False
+
+    if total >= limit != 0:
+        self.status.setText('Full')
+        if not sent:
+            if direct == 'lr':
                 lineAlert(context.format(self.cameranum, getin, getout, total, limit))
-                # information_box('This entrance{} has reached the limit!!'.format(self.cameranum), 0)
             else:
-                self.status.setText('Good')
+                lineAlert(context.format(self.cameranum, getout, getin, total, limit))
 
+        sent = True
+        # information_box('This entrance{} has reached the limit!!'.format(self.cameranum), 0)
